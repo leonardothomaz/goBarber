6552fa8de6fd53cc2b327289749c0586537c17c1
Leonard Thomaz <leonardo.thomaz@gmail.com>
2021-02-21 22:21:02 -0300

    Alterações nas migrations e adicionadas chaves estrangeiras entra a tabela de Usuário e de agendamento

--- goBarber/back-end/src/database/migrations/1613954484401-CreateUsers.ts
+++ goBarber/back-end/src/database/migrations/1613954484401-CreateUsers.ts
@@ -8,7 +8,7 @@ export default class CreateUsers1613954484401 implements MigrationInterface {
         columns: [
           {
             name: "id",
-            type: "varchar",
+            type: "uuid",
             isPrimary: true,
             generationStrategy: "uuid",
             default: "uuid_generate_v4()",
